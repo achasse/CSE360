be50417e6e49deda2ab264e7bf7e81f0146a89a8
lordhans <sellerhans@gmail.com>
2014-10-29 02:43:41 -0700

    Test
    Test

--- image_space_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from image_space_app.models import UserProfile
-
-# Register your models here.
-admin.site.register(UserProfile)
--- image_space_app/static/js/home.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $('.latest_pictures').slick({
-    slidesToShow: 1,
-    autoplay: true
-  });
-});
