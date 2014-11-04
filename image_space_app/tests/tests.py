778b4d80dbf3b0835199cd9fead40676d3d66a3a
Aaron <achasse@asu.edu>
2014-11-04 13:16:27 -0700

    More unit tests

--- image_space_app/tests/tests.py
+++ image_space_app/tests/tests.py
@@ -2,7 +2,8 @@ import datetime
 
 from django.utils import timezone
 from django.test import TestCase
-
+from image_space_app.views import *
+from django.core.urlresolvers import resolve
 
 
 
@@ -21,5 +22,37 @@ class ImageSpaceTests(TestCase):
         login = self.c.login(username = 'user1', password = 'password')
         self.assertTrue(login)
 
-    #def test_register(self):
-        #self.user
+    #Test that '/' goes to RootView view
+    def test_root(self):
+        res = resolve('/')
+        self.assertEqual(res.func, RootView)
+
+    #Test that '/login' goes to LoginUserView view
+    def test_login_view(self):
+        res = resolve('/login/')
+        self.assertEqual(res.func, LoginUserView)
+
+    #Test that '/register/' goes to RegisterView view
+    def test_register_view(self):
+        res = resolve('/register/')
+        self.assertEqual(res.func, RegisterView)
+
+    #Test that '/profile/' goes to ProfileDetails view
+    def test_profiledetails_view(self):
+        res = resolve('/profile/')
+        self.assertEqual(res.func, ProfileDetails)
+            
+
+    #Test logout (first with an active user logged in)
+    def test_logout_currently_logged_in(self):
+        self.user = User.objects.create(username = 'user2', password = 'password', is_active = True, is_superuser = False)
+        self.user = authenticate(username = 'user2', password = 'password')
+        login = self.c.login(username = 'user2', password = 'password')
+        logout()
+        self.assertTrue(logout)
+
+    #Test logout request when no user logged in
+    def test_logout_no_active_user(self):
+        logout()
+
+    
