76f2f4922f526c5096a965f205b4580c945e20eb
Aaron <achasse@asu.edu>
2014-11-04 12:54:05 -0700

    Added a login test case

--- image_space_app/tests/tests.py
+++ image_space_app/tests/tests.py
@@ -13,3 +13,13 @@ class ImageSpaceTests(TestCase):
         Dummy test
         """
         self.assertEqual(False, False)
+
+    #Test ability for an existing user to log in (user is created then authenticated)
+    def test_login(self):
+        self.user = User.objects.create(username = 'user1', password = 'password', is_active = True, is_superuser = False)
+        self.user = authenticate(username = 'user1', password = 'password')
+        login = self.c.login(username = 'user1', password = 'password')
+        self.assertTrue(login)
+
+    #def test_register(self):
+        #self.user
